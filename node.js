8097e1d7800080dfb62855cc304bd15454848df8
Shokha Denis <dejavu1231765@gmail.com>
2026-04-19 16:52:22 +0200

    update

---
+++ node.js
@@ -0,0 +1,21 @@
+﻿const fs = require('fs');
+const path = require('path');
+const saveFile = path.resolve(__dirname, 'saveData.json');
+
+function saveProgress(data) {
+  const json = JSON.stringify(data, null, 2);
+  fs.writeFileSync(saveFile, json, 'utf8');
+  console.log('Данные сохранены в', saveFile);
+}
+
+const example = {
+  date: new Date().toISOString(),
+  currentStage: 1,
+  answers: {
+    stage1: '',
+    stage2: '',
+    stage3: ''
+  }
+};
+
+saveProgress(example);
